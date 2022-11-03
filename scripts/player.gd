f5e6c4e805201e1824dd57fcce76eebe20ebf171
Prachi Joshi <josprachi@yahoo.com>
2022-11-03 21:36:13 +0530

    plot and players defined

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+extends AnimatedSprite
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	play("run")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+#	if Input.is_action_pressed("moveleft"):
+#		flip_h=true
+#		position.x-=10
+#	if Input.is_action_pressed("moveright"):
+#		flip_h=false
+#		position.x+=10	
+#	if Input.is_key_pressed(KEY_UP):
+#		position.y-=30	
+	pass
